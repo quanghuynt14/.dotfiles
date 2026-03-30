b87ccdcf095015e49f0b53428b0538977e463aa7
Huy <quanghuynt14@gmail.com>
2026-03-30 22:04:59 +0000

    updates

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,11 +6,3 @@ function fish_title
     echo (basename (pwd))" - "(status current-command)
 end
 
-# sentry
-fish_add_path "/Users/quanghuy/.sentry/bin"
-
-set -x GPG_TTY (tty)
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
