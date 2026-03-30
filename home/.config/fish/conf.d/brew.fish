960b1e5a85ac9fd567df688c9048ac91094847a9
Huy <quanghuynt14@gmail.com>
2026-03-30 21:24:04 +0000

    updates

--- home/.config/fish/conf.d/brew.fish
+++ home/.config/fish/conf.d/brew.fish
@@ -1,1 +1,7 @@
-eval (/opt/homebrew/bin/brew shellenv)
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+if test -x /home/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
