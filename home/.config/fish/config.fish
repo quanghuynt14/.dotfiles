5e9844b18d2634804ce3bfa21e5632e912673d86
Huy <quanghuynt14@gmail.com>
2026-03-30 20:24:46 +0200

    update configs

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,2 +1,16 @@
 # Disable greeting
 set fish_greeting 
+
+# fish title
+function fish_title
+    echo (basename (pwd))" - "(status current-command)
+end
+
+# sentry
+fish_add_path "/Users/quanghuy/.sentry/bin"
+
+set -x GPG_TTY (tty)
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
