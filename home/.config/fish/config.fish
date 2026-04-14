b92988e2d291e8e2ecb9392f75d2f1ded13a0a99
Huy <quanghuynt14@gmail.com>
2026-04-14 11:59:22 +0000

    add hostname

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,3 +6,10 @@ function fish_title
     echo (basename (pwd))" - "(status current-command)
 end
 
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# opencode
+fish_add_path /home/huy/.opencode/bin
